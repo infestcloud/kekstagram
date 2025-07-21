2d2c1fac7e5b3fec8022ca92b0561f3497240030
i5t4b3r <i5t4b3r@gmail.com>
2025-07-21 21:47:15 +0300

    Задание: Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const checkStringLength = (string, limit) => string.length < limit;
+
+checkStringLength('init', 3);
+getRandomPositiveInteger(1, 255);
